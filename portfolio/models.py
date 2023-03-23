bea7f65496a11943ce3fa6d2e723921c61081665
AlMikryukov <9127771067@mail.ru>
2023-03-23 10:18:34 +0500

    23.02

---
+++ portfolio/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Poject(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='portfolio/images')
+    url = models.URLField(blank=True)
